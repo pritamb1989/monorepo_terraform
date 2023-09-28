37d6a9fc18e33f781822fa8439af9ea69a4cc99f
Pritam Biswas <pritam.biswas@ah.nl>
2023-09-28 15:01:41 +0200

    terraform

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  description = "(Required) Resource Group Name"
+  type        = string
+}
+
+variable "location" {
+  description = "(Required) Azure Region to use"
+  type        = string
+}
